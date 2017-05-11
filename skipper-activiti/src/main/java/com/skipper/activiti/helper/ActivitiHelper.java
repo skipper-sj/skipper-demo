cb1b3ff571787933db75d6394fe6de3cb9ec3c2c
寿剑 <shoujian@shoujiandeMacBook-Pro.local>
2017-05-12 01:34:00 +0800

    commi

---
+++ skipper-activiti/src/main/java/com/skipper/activiti/helper/ActivitiHelper.java
@@ -0,0 +1,25 @@
+package com.skipper.activiti.helper;
+
+import javafx.application.Application;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by shoujian on 2017/5/12.
+ */
+public class ActivitiHelper {
+
+    private ProcessEngine processEngine;
+    private TaskService taskService;
+    private RuntimeService runtimeService;
+    private RepositoryService repositoryService;
+
+    public ActivitiHelper(){
+        processEngine = ApplicationContext.
+    }
+
+
+}
---
+++ skipper-helper/src/main/java/com.skipper.helper/util/SpringContext.java
@@ -0,0 +1,34 @@
+package com.skipper.helper.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * ApplicationContext的帮助类 自动装载ApplicationContext.
+ * <p>
+ * Created by shoujian on 2017/5/12.
+ */
+@Component
+public class SpringContext implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    /*
+	 * 注入ApplicationContext
+	 */
+    @Override
+    public void setApplicationContext(final ApplicationContext context) throws BeansException {
+        SpringContext.context = context;
+    }
+
+    public static Object getBean(final String beanName) {
+        return SpringContext.context.getBean(beanName);
+    }
+
+    public static Object getBean(final Class<?> clz) {
+        return context.getBean(clz);
+    }
+
+}
